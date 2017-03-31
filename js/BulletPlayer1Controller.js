9750cd9f295952ed4cf4ddf29665d0b447dc98c3
trinhthang2212 <trinhthang1122@gmail.com>
2017-03-31 14:14:40 +0700

    them BulletController va PlayerController, fix huong bullet

--- js/BulletPlayer1Controller.js
+++ js/BulletPlayer1Controller.js
@@ -1,27 +1,5 @@
-class BulletPlayer1Controller {
-    constructor(position, direction, bulletName, angle) {
-
-        // this.sprite = bulletPlayer1Group.create(position.x, position.y, 'assets', 'BulletType2.png');
-        this.sprite = bulletPlayer1Group.create(position.x, position.y, 'bullet');
-        this.sprite.anchor = new Phaser.Point(0.5, 0.5);
-        this.sprite.body.setSize(16, 16);
-        this.sprite.checkWorldBounds = true;
-        this.sprite.outOfBoundsKill = true;
-        this.sprite.angle = angle;
-        this.sprite.angleOffset = 90;
-        this.sprite.body.velocity = direction.setMagnitude(BulletPlayer1Controller.BULLET_SPEED);
-        bullets.push(this);
-        this.sprite.oneKilled = this.oneKilled;
-    }
-
-    update() {
-        if (!this.sprite.alive) {
-            var index = bullets.indexOf(this);
-            if (index != -1) {
-                bullets.splice(index, 1);
-            }
-        }
-    }
+class BulletPlayer1Controller extends BulletController{
+  constructor(position, direction){
+    super(position, direction, bulletPlayer1Group, 'bullet');
+  }
 }
-
-BulletPlayer1Controller.BULLET_SPEED = 250;
--- js/BulletPlayer2Controller.js
+++ js/BulletPlayer2Controller.js
@@ -1,27 +1,5 @@
-class BulletPlayer2Controller {
-    constructor(position, direction, bulletName, angle) {
-
-        // this.sprite = bulletPlayer1Group.create(position.x, position.y, 'assets', 'BulletType2.png');
-        this.sprite = bulletPlayer2Group.create(position.x, position.y,'bullet');
-        this.sprite.anchor = new Phaser.Point(0.5, 0.5);
-        this.sprite.body.setSize(16, 16);
-        this.sprite.checkWorldBounds = true;
-        this.sprite.outOfBoundsKill = true;
-        this.sprite.angle = angle;
-        this.sprite.angleOffset = 90;
-        this.sprite.body.velocity = direction.setMagnitude(BulletPlayer2Controller.BULLET_SPEED);
-        bullets.push(this);
-        this.sprite.oneKilled = this.oneKilled;
-    }
-
-    update() {
-          if (!this.sprite.alive) {
-            var index = bullets.indexOf(this);
-            if (index != -1) {
-              bullets.splice(index,1);
-            }
-          }
-    }
+class BulletPlayer2Controller extends BulletController{
+  constructor(position, direction){
+    super(position, direction, bulletPlayer2Group, 'bullet');
+  }
 }
-
-BulletPlayer2Controller.BULLET_SPEED = 250;
